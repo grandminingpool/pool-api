83aa9ff70a0df5755d28cbebbf0c9b8f4200a928
Viktor Schastnyy <victor92sh@gmail.com>
2024-11-13 17:16:26 +0300

    feat: allowed cors for get and options methods

--- internal/api/server/server.go
+++ internal/api/server/server.go
@@ -1,15 +1,45 @@
 package apiServer
 
 import (
+	"net/http"
+	"strings"
+
 	apiModels "github.com/grandminingpool/pool-api/api/generated"
 	serverHandelrs "github.com/grandminingpool/pool-api/internal/common/server/handlers"
 )
 
-func CreateServer(h apiModels.Handler) (*apiModels.Server, error) {
-	return apiModels.NewServer(
+type Server struct {
+	srv *apiModels.Server
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join([]string{
+		http.MethodGet,
+		http.MethodOptions,
+	}, ", "))
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+
+		return
+	}
+
+	s.srv.ServeHTTP(w, r)
+}
+
+func CreateServer(h apiModels.Handler) (*Server, error) {
+	srv, err := apiModels.NewServer(
 		h,
 		apiModels.WithErrorHandler(serverHandelrs.ErrorHandler()),
 		apiModels.WithMethodNotAllowed(serverHandelrs.MethodNotAllowedHandler()),
 		apiModels.WithNotFound(serverHandelrs.NotFoundHandler()),
 	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Server{
+		srv: srv,
+	}, nil
 }
